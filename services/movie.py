e1acb1fd79df3e20c308bd0733d260387341ad2a
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-06-30 12:33:31 +0300

    fix annotations

--- services/movie.py
+++ services/movie.py
@@ -1,10 +1,12 @@
+from django.db.models import QuerySet
+
 from db.models import Movie
 
 
 def get_movies(
         genres_ids: list[int] = None,
         actors_ids: list[int] = None
-) -> list:
+) -> QuerySet:
     movies = Movie.objects.all()
     if genres_ids and actors_ids:
         movies = movies.filter(
@@ -18,7 +20,7 @@ def get_movies(
     return movies
 
 
-def get_movie_by_id(movie_id: int) -> None:
+def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
--- services/movie_session.py
+++ services/movie_session.py
@@ -47,4 +47,4 @@ def update_movie_session(
 
 
 def delete_movie_session_by_id(session_id: int) -> tuple:
-    return MovieSession.objects.get(id=session_id).delete()
+    return get_movie_session_by_id(session_id).delete()
